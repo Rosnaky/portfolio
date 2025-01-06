c1a4ec386245ddebb98b2a9574199fc77dcd3421
Rosnaky <ronakjpatelr22@gmail.com>
2025-01-05 22:51:55 -0500

    added name and headline to title screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import SplashScreen from "./splash_screen";
+import TitleScreen from "./title_screen";
+import Head from "next/head";
 
 const Home: React.FC = () => {
   const [isSplashVisible, setIsSplashVisible] = useState<boolean>(true);
@@ -10,12 +12,14 @@ const Home: React.FC = () => {
 
   return (
     <>
+      <Head>
+        <title>Ronak Patel</title>
+        <meta name="description" content="Ronak Patel's Portfolio" />
+      </Head>
       {isSplashVisible ? (
         <SplashScreen onFinish={handleSplashFinish} />
       ) : (
-        <main>
-          <div>Ronak</div>
-        </main>
+        <TitleScreen />
       )}
     </>
   )
--- src/app/title_screen.tsx
+++ src/app/title_screen.tsx
@@ -1,15 +1,26 @@
-import React, { useRef, useEffect } from 'react';
+import styles from "./title_screen.module.css"
 
 const TitleScreen: React.FC = () => {
-    const containerRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            // Three.js scene
-        }
-    }, []);
+    return (
+        <div className={`${styles.titleContainer} " grid grid-rows-4 grid-cols-6 gap-4"`}>
+            <div className="row-start-2 row-end-4 col-start-2 col-end-4 ">
+                <div className="flex gap-8 items-center font-semibold py-2 text-8xl">
+                    <div className="text-teal-200">
+                        RONAK
+                    </div>
+                    <div>
+                        PATEL
+                    </div>
+                </div>
 
-    return <div ref={containerRef} />;
-};
+                <div className="text-5xl">
+                    Software Engineering @ UWaterloo
+                </div>
+            </div>
+        </div>
+    )
+
+}
 
 export default TitleScreen;
