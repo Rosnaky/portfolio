3842f0eb8d41285ea4de68d3936873dd10e09cdd
Rosnaky <ronakjpatelr22@gmail.com>
2025-02-08 17:01:54 -0500

    afacad as global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Afacad_Flux } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const AfacadFlux = Afacad_Flux({
+  variable: "--font-afacad-flux",
+  subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "Ronak Patel",
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${AfacadFlux.className} antialiased`}
       >
         {children}
       </body>
