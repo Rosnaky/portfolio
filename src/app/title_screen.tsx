5b70e830d3a1dfae447222445963fb722c8bdcca
Rosnaky <ronakjpatelr22@gmail.com>
2025-02-18 21:00:11 -0500

    responsive title

--- src/app/title_screen.tsx
+++ src/app/title_screen.tsx
@@ -14,15 +14,18 @@ const TitleScreen: React.FC = () => {
             <div className={`${styles.windowDrop} z-10`}></div>
 
             {/* Title Container */}
-            <div className={`relative ${styles.titleContainer}  grid grid-rows-6 grid-cols-6 gap-4 z-20`}>
-                <div className="row-start-2 row-end-4 col-start-2 col-end-4">
-                    <div className="flex gap-8 items-center font-extrabold py-2 text-8xl">
+            <div className={`relative ${styles.titleContainer} grid grid-rows-6 grid-cols-6 gap-4 z-20`}>
+                <div className="row-start-2 row-end-5 col-start-2 col-end-5 flex flex-col items-center md:items-start text-center md:text-left">
+                    <div className="flex flex-col md:flex-row gap-2 md:gap-8 items-center font-extrabold text-5xl sm:text-6xl md:text-7xl lg:text-8xl py-2">
                         <div className="text-teal-200">RONAK</div>
                         <div>PATEL</div>
                     </div>
-                    <div className="text-3xl">Software Engineering @ UWaterloo</div>
+                    <div className="text-xl sm:text-2xl md:text-3xl">
+                        Software Engineering @ UWaterloo
+                    </div>
                 </div>
             </div>
+
             <ScrollIndicator/>
         </div>
     );
