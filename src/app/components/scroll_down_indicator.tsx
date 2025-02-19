cbd17b89e589620eeff5bd4e722835aa6ed3411a
Rosnaky <ronakjpatelr22@gmail.com>
2025-02-18 23:51:48 -0500

    scroll indicator actually scrolls

--- src/app/components/scroll_down_indicator.tsx
+++ src/app/components/scroll_down_indicator.tsx
@@ -2,8 +2,16 @@ import { motion } from "framer-motion";
 import { ChevronDown } from "lucide-react";
 
 const ScrollIndicator = () => {
+
+  const scroll = () => {
+    window.scrollBy({ top:window.innerHeight, behavior: "smooth" })
+  }
+
   return (
-    <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
+    <div 
+      className="absolute bottom-10 left-1/2 transform -translate-x-1/2 flex flex-col items-center"
+      onClick={scroll}
+    >
       <motion.div
         animate={{ y: [0, 10, 0] }}
         transition={{ repeat: Infinity, duration: 1.5, ease: "easeInOut" }}
